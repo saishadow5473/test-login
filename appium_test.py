2b071d86472ce79c08db5b74510146331c78929d
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-09-27 11:49:17 +0530

    Update appium_test.py

--- appium_test.py
+++ appium_test.py
@@ -1,8 +1,8 @@
-import time
 from appium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # Desired capabilities for your Flutter app
 desired_caps = {
