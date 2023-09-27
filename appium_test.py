73d8aa8039db2cda91ece5ebf8517c867501ea38
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-09-27 12:14:34 +0530

    Update appium_test.py

--- appium_test.py
+++ appium_test.py
@@ -1,14 +1,11 @@
-from appium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+from appium import webdriver
 
 # Desired capabilities for your Flutter app
 desired_caps = {
     'platformName': 'Android',
     'deviceName': 'mytempavd',
-    'app': '/home/vsts/work/1/s/build/app/outputs/flutter-apk/app-release.apk'
+    'app': '/home/vsts/work/1/s/build/app/outputs/flutter-apk/app-release.apk',
 }
 
 # Initialize the Appium driver
@@ -16,23 +13,14 @@ driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 # Define a function to perform login
 def perform_login(email, password):
-    # Wait for the email input field to be visible
-    email_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'email_input_id'))
-    )
-    email_input.send_keys(email)
+    # Wait for the email input field to be visible and interact with it
+    driver.find_element_by_accessibility_id('email_input_id').send_keys(email)
 
-    # Wait for the password input field to be visible
-    password_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'password_input_id'))
-    )
-    password_input.send_keys(password)
+    # Wait for the password input field to be visible and interact with it
+    driver.find_element_by_accessibility_id('password_input_id').send_keys(password)
 
-    # Wait for the Submit button to be clickable
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ACCESSIBILITY_ID, 'submit_button_id'))
-    )
-    submit_button.click()
+    # Wait for the Submit button to be clickable and click it
+    driver.find_element_by_accessibility_id('submit_button_id').click()
 
 # Test login functionality
 try:
