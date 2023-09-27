8ff73441b14ff183b592ecc86152c1df00719650
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-09-27 13:28:36 +0530

    Update appium_test.py

--- appium_test.py
+++ appium_test.py
@@ -6,7 +6,7 @@ from time import sleep
 desired_caps = {
     'platformName': 'Android',
     'deviceName': 'mytempavd',  # Replace with your target device name
-    'app': '/home/vsts/work/1/s/build/app/outputs/flutter-apk/app-release.apk'
+    'app': '/home/vsts/work/1/s/build/app/outputs/flutter-apk/app-release.apk',
     'automationName': 'UiAutomator2'
 }
 
@@ -20,23 +20,23 @@ driver = webdriver.Remote(server_url, desired_caps)
 sleep(5)
 
 # Find the email and password text fields
-email_field = driver.find_element_by_id("emailController")
-password_field = driver.find_element_by_id("passwordController")
+email_field = driver.find_element_by_id("email_input_id")  # Assuming this is the correct ID
+password_field = driver.find_element_by_id("password_input_id")  # Assuming this is the correct ID
 
 # Enter the login credentials
 email_field.send_keys("arun@gogosoon.com")
 password_field.send_keys("qazxswedcvfr")
 
 # Perform the login action
-submit_button = driver.find_element_by_id("submitButton")
+submit_button = driver.find_element_by_id("submit_button_id")  # Assuming this is the correct ID
 submit_button.click()
 
 # Wait for the login process to complete
 sleep(5)
 
 # Verify if the login was successful
-welcome_text = driver.find_element_by_id("emailText").text
-if welcome_text == "Welcome, arun@gogosoon.com!":
+welcome_text = driver.find_element_by_id("homepageText")  # Assuming this is the correct ID
+if welcome_text.text == "Welcome, arun@gogosoon.com!":
     print("Login Successful!")
 else:
     print("Login Failed!")
